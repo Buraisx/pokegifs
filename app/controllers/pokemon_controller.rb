88738bd9b534bfeeed9ec87711d3790839765289
Situ William <situ_william@yahoo.com>
2017-12-16 17:23:20 -0500

    partial fix to team route

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -24,16 +24,17 @@ class PokemonController < ApplicationController
 		}
 	end
 	def team
+		# THIS DOES NOT WORK
 		6.times do
 			pokemon = Random.new
-			res  = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{pokemon.rand(600)}")
+			res  = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{pokemon.rand(600).to_s}")
 			body = JSON.parse(res.body)
 			name = body["name"]
 			id = body["id"]
 			type = body["types"][0]["type"]["name"]
 
 
-			res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=aHtC6smrcaWbfwhKHkW5m1pJ0e1Ae4Ke&q=#{pokemon.rand(600)}&rating=g")
+			res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=aHtC6smrcaWbfwhKHkW5m1pJ0e1Ae4Ke&q=pokemon&rating=g")
 			body = JSON.parse(res.body)
 			gif_url = body["data"][0]["url"]
 
