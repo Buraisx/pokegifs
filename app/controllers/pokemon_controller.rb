d032ce12378ae85d08a345b3e82b6e32b8192909
Situ William <situ_william@yahoo.com>
2017-12-15 19:38:09 -0500

    added searchable pokemon to controller

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,50 @@
+class PokemonController < ApplicationController
+	def index
+		render json:{"message": "ok"}
+	end
+	def show
+		res  = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}")
+		body = JSON.parse(res.body)
+		name = body["name"]
+		id = body["id"]
+		type = body["types"][0]["type"]["name"]
+
+
+		res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=aHtC6smrcaWbfwhKHkW5m1pJ0e1Ae4Ke&q=#{params[:id]}&rating=g")
+		body = JSON.parse(res.body)
+		gif_url = body["data"][0]["url"]
+
+		render json: 
+		{
+			"id": id,
+			"name": name,
+			"type": type,
+			"gif": gif_url
+
+		}
+	end
+	def team
+		6.times do
+			pokemon = Random.new
+			res  = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{pokemon.rand(600)}")
+			body = JSON.parse(res.body)
+			name = body["name"]
+			id = body["id"]
+			type = body["types"][0]["type"]["name"]
+
+
+			res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=aHtC6smrcaWbfwhKHkW5m1pJ0e1Ae4Ke&q=#{pokemon.rand(600)}&rating=g")
+			body = JSON.parse(res.body)
+			gif_url = body["data"][0]["url"]
+
+			render json: 
+			{
+				"id": id,
+				"name": name,
+				"type": type,
+				"gif": gif_url
+
+			}
+		end
+	end
+end
